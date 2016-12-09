9f3ed55f693db8ede4c15d7a7a78c1577c59b9a0
zhangruizhou <zrzqq@sina.cn>
2016-12-09 10:54:22 +0800

    login

---
+++ app/Http/Controllers/Login/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Login;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends BaseController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
