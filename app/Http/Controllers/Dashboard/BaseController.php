c5e1c353ce901ffbf3ca3c4f05c353526efab992
zhangruizhou <zrzqq@sina.cn>
2016-12-09 17:04:56 +0800

    login

---
+++ app/Http/Controllers/Dashboard/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+}
---
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Dashboard\BaseController;
+
+class IndexController extends BaseController {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+        echo 'index';
+    }
+}
