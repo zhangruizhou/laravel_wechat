c02adbb6f18e875eeb76aca2cdc3bbf3cc3516b7
zhangruizhou <zrzqq@sina.cn>
2016-12-12 11:17:49 +0800

    login

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,10 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
 Route::get('/home', 'HomeController@index');
 
